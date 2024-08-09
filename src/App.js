75fd76b714ec129c3b26544d53126bfad6222741
navaneethmadhav <madnav114@gmail.com>
2024-08-09 23:30:15 +0530

    designign  dashboard section

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import MainSection from './Components/MainSection';
+import SearchSection from './Components/SearchSection';
+
+function App() {
+  return (
+    <div className="App">
+      <SearchSection />
+      <MainSection />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/MainSection.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import '../Styles/MainSection.css'
+import expiredIcon from '../Assets/Icons/Expired.svg'
+import activeIcon from '../Assets/Icons/activeTask.svg'
+import completedIcon from '../Assets/Icons/clock.svg'
+
+const MainSection = () => {
+    return (
+        <div className='dashboard-body-container'>
+            <div className="dashboard-sidebar">
+                <div className="sidebar-section-block">
+                    <div className="section-block-image1">
+                        <img src={expiredIcon} alt="" />
+                    </div>
+                    <h5>Expired Tasks</h5>
+                    <h1>5</h1>
+                </div>
+
+                <div className="sidebar-section-block">
+                    <div className="section-block-image2">
+                        <img src={activeIcon} alt="" />
+                    </div>
+                    <h5>Expired Tasks</h5>
+                    <h1>5</h1>
+                </div>
+
+                <div className="sidebar-section-block">
+                    <div className="section-block-image3">
+                        <img src={completedIcon} alt="" />
+                    </div>
+                    <h5>Expired Tasks</h5>
+                    <h1>5</h1>
+                </div>
+            </div>
+            <div className="dashboard-list-section"></div>
+        </div>
+    )
+}
+
+export default MainSection
